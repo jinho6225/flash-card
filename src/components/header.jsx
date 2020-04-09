f4b2cc34b3227f2f92cdf8295122ec45eb4f8295
jinho6225 <jinho6225@hotmail.com>
2020-04-09 00:11:21 +0000

    added header and footer

---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Nav from './nav.jsx';
+
+const Header = (props) => {
+  const { editing, isEditing, setView, view } = props;
+  return (
+    <div className="bgBlack text-white sticky-top py-3">
+      <div className="container d-flex align-items-center justify-content-between">
+        <h3 className="text-light m-0 title pointer">🚀 Flash Card</h3>
+        <Nav
+          editing={editing}
+          isEditing={isEditing}
+          setView={setView}
+          view={view}
+        />
+      </div>
+    </div>
+  );
+};
+export default Header;
